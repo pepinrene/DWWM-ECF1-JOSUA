469685ab65584a5624398af3bacf5cf57f7ded0a
pépinrène <pepin.j.rene@gmail.com>
2025-08-28 12:01:42 +0200

    AJOURS

--- app.js
+++ app.js
@@ -43,3 +43,29 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("js-audio");
+  const playBtn = document.getElementById("js-play-btn");
+  const eq = document.getElementById("js-equalizer");
+
+  playBtn.addEventListener("click", () => {
+    if (audio.paused) {
+      audio
+        .play()
+        .then(() => {
+          eq.classList.add("equalizer--playing");
+          playBtn.textContent = "Stop";
+        })
+        .catch((err) => console.error("Erreur de lecture audio :", err));
+    } else {
+      audio.pause();
+      eq.classList.remove("equalizer--playing");
+      playBtn.textContent = "Play";
+    }
+  });
+  audio.addEventListener("ended", () => {
+    eq.classList.remove("equalizer--playing");
+    playBtn.textContent = "Play";
+  });
+});
